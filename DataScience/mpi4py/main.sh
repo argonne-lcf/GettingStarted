dc38be1c333deeebc4733f362d5a21840493b708
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2023-01-16 22:41:01 +0000

    Moved files to respective folders

--- DataScience/mpi4py/main.sh
+++ DataScience/mpi4py/main.sh
@@ -4,5 +4,6 @@ conda activate base
 NRANKS=$(wc -l < "${PBS_NODEFILE}")
 NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
 NGPUS="$((${NRANKS}*${NGPU_PER_RANK}))"
+cd ${PBS_O_WORKDIR}
 mpiexec --envall -n "${NGPUS}" --ppn "${NGPU_PER_RANK}" --hostfile="${PBS_NODEFILE}" python3 main.py
 
