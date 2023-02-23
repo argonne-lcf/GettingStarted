3d788d71562d4025704bf55a924175e2cf4d91f4
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2023-02-23 16:31:10 +0000

    Update Polaris job submission script

--- DataScience/Containers/mpi/Polaris/job_submission.sh
+++ DataScience/Containers/mpi/Polaris/job_submission.sh
@@ -4,37 +4,39 @@
 #PBS -l place=scatter
 #PBS -l walltime=0:30:00
 #PBS -l filesystems=home:grand
-#PBS -A Datascience
+#PBS -A <PROJECT_NAME>
 
 cd ${PBS_O_WORKDIR}
 echo $CONTAINER
 
 # SET proxy for internet access
-module load conda
 module load singularity
 export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
 export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
 export http_proxy=http://proxy.alcf.anl.gov:3128
 export https_proxy=http://proxy.alcf.anl.gov:3128
 
+# Needed for Polaris PE to map to Singularity
+ADDITIONAL_PATH=/opt/cray/pe/pals/1.1.7/lib/
+
+module load cray-mpich-abi
+
+
 # MPI example w/ 16 MPI ranks per node spread evenly across cores
 NODES=`wc -l < $PBS_NODEFILE`
-PPN=1
+PPN=16
 PROCS=$((NODES * PPN))
 echo "NUM_OF_NODES= ${NODES} TOTAL_NUM_RANKS= ${PROCS} RANKS_PER_NODE= ${PPN}"
 
-MPI_BASE=/opt/nvidia/hpc_sdk/Linux_x86_64/21.9/comm_libs/hpcx/hpcx-2.9.0/ompi/
-export PATH=$MPI_BASE/bin:$PATH
-export LD_LIBRARY_PATH=$MPI_BASE/lib:$LD_LIBRARY_PATH
-export SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-echo mpirun=$(which mpirun)
+
+export SINGULARITYENV_LD_LIBRARY_PATH="$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH:$ADDITIONAL_PATH"
 
 # Openmpi Does not work on Polaris
-echo library path
-mpirun -hostfile $PBS_NODEFILE -n $PROCS -npernode $PPN singularity exec --nv -B $MPI_BASE $CONTAINER ldd /usr/source/mpi_hello_world
+#echo library path
+#mpiexec -hostfile $PBS_NODEFILE -n $PROCS -ppn $PPN singularity exec -B /opt -B /var/run/palsd/ $CONTAINER ldd /usr/source/mpi_hello_world
 
 echo C++ MPI
-mpirun -hostfile $PBS_NODEFILE -n $PROCS -npernode $PPN singularity exec --nv -B $MPI_BASE $CONTAINER /usr/source/mpi_hello_world
+mpiexec -hostfile $PBS_NODEFILE -n $PROCS -ppn $PPN singularity exec -B /opt -B /var/run/palsd/ $CONTAINER /usr/source/mpi_hello_world
 
 echo Python MPI
-mpirun -hostfile $PBS_NODEFILE -n $PROCS -npernode $PPN singularity exec --nv -B $MPI_BASE $CONTAINER python3 /usr/source/mpi_hello_world.py
+mpiexec -hostfile $PBS_NODEFILE -n $PROCS -ppn $PPN singularity exec -B /opt -B /var/run/palsd/ $CONTAINER python3 /usr/source/mpi_hello_world.py
