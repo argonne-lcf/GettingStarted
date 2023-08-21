a62e50440b00d110b1fb4e4df34020711ae9188a
cjknight <cjknight2009@gmail.com>
2023-08-21 17:41:42 +0000

    simple SYCL example for Polaris using GPU-aware MPI

--- ProgrammingModels/Polaris/SYCL/vecadd_mpi/README.md
+++ ProgrammingModels/Polaris/SYCL/vecadd_mpi/README.md
@@ -1,28 +1,53 @@
-# Cray Compiler Wrappers
-CUDA applications can be built on Polaris using the NVIDIA programming environment using appropriate modules. The default programming environment is `PrgEnv-nvidia`. All of the examples below are built and run within interactive jobs on Polaris nodes.
+# OneAPI Compiler Wrappers
+SYCL applications can be built on Polaris using the oneapi module and compiler wrappers provided by ALCF. These can be loaded into the environment using the following commands. 
 
-# Compilation w/ NVIDIA compilers
+```
+module restore
+module load oneapi
+module load mpiwrappers/cray-mpich-oneapi
+```
+
+The `module restore` command is a quick way to restore the default environment. All of the examples below are built and run within interactive jobs on Polaris nodes.
+
+# Compilation w/ SYCL compiler
 ```
 qsub -I -l select=1,walltime=1:00:00 -q debug -l filesystems=home:grand:eagle -A <PROJECT>
 
-$ module load craype-accel-nvidia80
-$ make -f Makefile.nvidia clean
-
-$ make -f Makefile.nvidia
-CC -g -O3 -std=c++0x -mp=gpu -gpu=cc80,cuda11.0 -c main.cpp
-CC -o vecadd -g -O3 -std=c++0x -mp=gpu -gpu=cc80,cuda11.0 main.o 
+$ module load oneapi
+$ module load mpiwrappers/cray-mpich-oneapi
+$ make clean
+$ make
+mpicxx -g -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80 -std=c++20 -c main.cpp
+mpicxx -o vecadd -g -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80 -std=c++20 main.o  -L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_cuda
 ```
 ## Compilation flags
-Important compilation flags for OpenMP with the NVIDIA compilers are `-mp=gpu -gpu=cc80,cuda11.0`.
+Important compilation flags for SYCL running on GPUs are `-fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80`. If using GPU-aware MPI, then the application needs to be linked to the appropriate gtl libraries `-L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_cuda`.
 
 ## Example output:
 ```
-$ mpiexec -n 4 ./vecadd
-# of devices= 4
+$ export MPICH_GPU_SUPPORT_ENABLED=1
+$ mpiexec -n 8 --ppn 8 --depth=4 --cpu-bind depth ./vecadd 
+List Platforms and Devices
+Platform: NVIDIA CUDA BACKEND NVIDIA Corporation CUDA 11.4
+-- Device: NVIDIA A100-SXM4-40GB is a gpu
+Platform: NVIDIA CUDA BACKEND NVIDIA Corporation CUDA 11.4
+-- Device: NVIDIA A100-SXM4-40GB is a gpu
+Platform: NVIDIA CUDA BACKEND NVIDIA Corporation CUDA 11.4
+-- Device: NVIDIA A100-SXM4-40GB is a gpu
+Platform: NVIDIA CUDA BACKEND NVIDIA Corporation CUDA 11.4
+-- Device: NVIDIA A100-SXM4-40GB is a gpu
+Rank 0 running on GPU 0!
 Rank 1 running on GPU 1!
 Rank 2 running on GPU 2!
 Rank 3 running on GPU 3!
-Rank 0 running on GPU 0!
+Rank 4 running on GPU 0!
+Rank 5 running on GPU 1!
+Rank 6 running on GPU 2!
+Rank 7 running on GPU 3!
+Running on 8 ranks
+# of devices= 4
+
+Using double-precision
 
 
 Result is CORRECT!! :)
--- ProgrammingModels/Polaris/SYCL/vecadd_mpi/main.cpp
+++ ProgrammingModels/Polaris/SYCL/vecadd_mpi/main.cpp
@@ -5,27 +5,21 @@
 #include <cassert>
 #include "mpi.h"
 
-#include <sycl/sycl.h>
+#include <sycl/sycl.hpp>
 
 #define _N 1024
 #define _LOCAL_SIZE 64
 
 #ifdef _SINGLE_PRECISION
   typedef float real_t;
+#define MY_MPI_TYPE MPI_FLOAT
 #else
   typedef double real_t;
+#define MY_MPI_TYPE MPI_DOUBLE
 #endif
 
-// ----------------------------------------------------------------
-
-void _vecadd(real_t * a, real_t * b, real_t * c, int N)
-{
-
-  for(int i=0; i<N; ++i) {
-    c[i] = a[i] + b[i];
-  }
-  
-}
+// is this really needed?
+#define ConvertToActualTypeSycl(Scalar, buf_acc) static_cast<Scalar*>(static_cast<void*>(((buf_acc.get_pointer().get()))))
 
 // ----------------------------------------------------------------
 						 
@@ -33,36 +27,65 @@ int main( int argc, char* argv[] )
 {
   MPI_Init(&argc, &argv);
 
-  int me,nranks;
-  MPI_Comm_size(MPI_COMM_WORLD, &nranks);
-  MPI_Comm_rank(MPI_COMM_WORLD, &me);
+  MPI_Comm world = MPI_COMM_WORLD;
+  
+  int me, nranks;
+  MPI_Comm_size(world, &nranks);
+  MPI_Comm_rank(world, &me);
 
   const int N = _N;
 
-  real_t * a = (real_t*) malloc(N*sizeof(real_t));
-  real_t * b = (real_t*) malloc(N*sizeof(real_t));
-  real_t * c = (real_t*) malloc(N*sizeof(real_t));
+  std::vector<real_t> a(N);
+  std::vector<real_t> b(N);
+  std::vector<real_t> c(N);
+  std::vector<real_t> d(N);
 
   // Initialize host
   for(int i=0; i<N; ++i) {
     a[i] = sin(i)*sin(i);
     b[i] = cos(i)*cos(i);
     c[i] = -1.0;
+    d[i] = 0.0;
   }
-  /*
-  int num_devices = omp_get_num_devices();
 
-  if(me == 0) {
-    printf("# of devices= %i\n",num_devices);
+  // https://github.com/argonne-lcf/sycltrain/blob/master/9_sycl_of_hell/0_tiny_sycl_info.cpp
+  
+  if(me == 0) std::cout << "List Platforms and Devices" << std::endl;
+  std::vector<sycl::platform> platforms = sycl::platform::get_platforms();
+  for (const auto &plat : platforms) {
+    // get_info is a template. So we pass the type as an `arguments`.
+    if(me == 0) std::cout << "Platform: "
+			  << plat.get_info<sycl::info::platform::name>() << " "
+			  << plat.get_info<sycl::info::platform::vendor>() << " "
+			  << plat.get_info<sycl::info::platform::version>() << std::endl;
+    // Trivia: how can we loop over argument?
+    
+    std::vector<sycl::device> devices = plat.get_devices();
+    for (const auto &dev : devices) {
+      if(me == 0) std::cout << "-- Device: "
+			    << dev.get_info<sycl::info::device::name>() << " "
+			    << (dev.is_gpu() ? "is a gpu" : " is not a gpu") << std::endl;
+      // sycl::info::device::device_type exist, one can use a Map to translate to a nice sting
+      // https://github.com/intel/llvm/blob/ef4e6ddce56fe21621bca7d62932713152de3f0e/sycl/source/detail/device_filter.cpp#L22
+    }
   }
+
+  // the following assumes each platform is a single GPU
+  
+  int num_devices = platforms.size();
 
   // Device ID
 
   int device_id = me % num_devices;
+
+  sycl::queue Q( platforms[device_id].get_devices()[0] );
+  
   for(int i=0; i<nranks; ++i) {
-    if(i == me) {
-      printf("Rank %i running on GPU %i!\n",me,device_id);
+    if(i == 0 && me == 0) {
+      printf("Running on %i ranks\n",nranks);
+      printf("# of devices= %i\n\n",num_devices);
     }
+    if(i == me) printf("Rank %i running on GPU %i!\n",me,device_id);
     MPI_Barrier(MPI_COMM_WORLD);
   }
   
@@ -73,18 +96,61 @@ int main( int argc, char* argv[] )
 #endif
 
   // Create device buffers and transfer data to device
-  
-#pragma omp target enter data map(alloc: a[:N], b[:N], c[:N] )
-#pragma omp target enter data map(to: a[:N], b[:N], c[:N] )
 
+  real_t * d_a = malloc_device<real_t>(N, Q);
+  real_t * d_b = malloc_device<real_t>(N, Q);
+  real_t * d_c = malloc_device<real_t>(N, Q);
+  real_t * d_d = malloc_device<real_t>(N, Q);
+
+  // Transfer host-to-device
+  
+  Q.memcpy(d_a, a.data(), N*sizeof(real_t));
+  Q.memcpy(d_b, b.data(), N*sizeof(real_t));  
+  Q.memcpy(d_c, c.data(), N*sizeof(real_t));
+  Q.memcpy(d_d, d.data(), N*sizeof(real_t));
+  Q.wait();
+  
+  int send = (me < nranks-1) ? me + 1: 0;
+  int recv = (me > 0) ? me - 1: nranks-1;
+
+  //  printf("(%i) sending to %i and receiving from %i\n",me,send,recv);
+  
+  // Zero array (silly use of MPI, but you get idea...)
+  
+  if(nranks > 1) {
+
+    // Initialize c array to zero using MPI and gpu buffers
+  
+    MPI_Request request;
+    MPI_Irecv(d_c, N, MY_MPI_TYPE, recv, 0, world, &request);
+    MPI_Send(d_d, N, MY_MPI_TYPE, send, 0, world);
+    MPI_Wait(&request, MPI_STATUS_IGNORE);
+    
+  } else {
+
+    Q.memcpy(d_c, d_d, N*sizeof(real_t));
+    
+  }
+  
   // Execute kernel
 
-  _vecadd(a, b, c, N);
+  Q.submit([&](sycl::handler &cgh) {
 
+    cgh.parallel_for(N, [=](sycl::id<1> idx) {
+      d_c[idx] += d_a[idx] + d_b[idx];
+	}
+      );
+    
+  });
+
+  Q.wait();
+  
   // Transfer data from device
 
-#pragma omp target exit data map(from: c[:N] )
+  Q.memcpy(c.data(), d_c, N*sizeof(real_t));
 
+  Q.wait();
+  
   //Check result on host
 
   double diff = 0;
@@ -95,15 +161,9 @@ int main( int argc, char* argv[] )
 
   // Clean up
 
-#pragma omp target exit data map(delete: a, b, c)
-  */
-  free(a);
-  free(b);
-  free(c);
-
-  int sum;
-  MPI_Reduce(&diffsq, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
+  double sum = 0;
+  MPI_Reduce(&diffsq, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  
   if(me == 0) {
     if(sum < 1e-6) printf("\nResult is CORRECT!! :)\n");
     else printf("\nResult is WRONG!! :(\n");
