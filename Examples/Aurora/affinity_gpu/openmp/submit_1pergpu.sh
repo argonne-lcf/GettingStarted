25aabecb639421787bd340d83ed168679d9d2953
Chris Knight <knight@x4520c6s0b0n0.hostmgmt.cm.aurora.alcf.anl.gov>
2024-02-01 05:00:23 +0000

    update submit scripts

--- Examples/Aurora/affinity_gpu/openmp/submit_1pergpu.sh
+++ Examples/Aurora/affinity_gpu/openmp/submit_1pergpu.sh
@@ -16,4 +16,9 @@ NTHREADS=1
 NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
 echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
 
-mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth ../../set_affinity_gpu_aurora.sh ./hello_affinity
+AFFINITY=""
+AFFINITY="../../../HelperScripts/Aurora/set_affinity_gpu_aurora.sh"
+
+COMMAND="mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth ${AFFINITY} ./hello_affinity"
+echo "COMMAND= ${COMMAND}"
+${COMMAND}
