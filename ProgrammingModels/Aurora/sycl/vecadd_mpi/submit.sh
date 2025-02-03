de816cb7ecb8b571509b8878c0b68cdbf1f483a4
Chris Knight <knight@aurora-uan-0010.hostmgmt.cm.aurora.alcf.anl.gov>
2025-02-03 05:05:22 +0000

    fix sycl submit.sh

--- ProgrammingModels/Aurora/sycl/vecadd_mpi/submit.sh
+++ ProgrammingModels/Aurora/sycl/vecadd_mpi/submit.sh
@@ -2,8 +2,9 @@
 #PBS -l select=1
 #PBS -l place=scatter
 #PBS -l walltime=0:15:00
-#PBS -q EarlyAppAccess
-#PBS -A Aurora_deployment
+#PBS -q debug
+#PBS -A Catalyst
+#PBS -l filesystems=home:flare
 
 cd ${PBS_O_WORKDIR}
 
@@ -17,9 +18,13 @@ NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
 echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
 
 EXE=./vecadd
-EXE="../../../../HelperScripts/Aurora/set_affinity_gpu_aurora.sh ${EXE}"
 
-COMMAND="mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth ${EXE}"
+MPI_ARG="-n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth "
+
+AFFINITY=""
+AFFINITY="../../../../HelperScripts/Aurora/set_affinity_gpu.sh"
+
+COMMAND="mpiexec ${MPI_ARG} ${AFFINITY} ${EXE}"
 
 echo "COMMAND= ${COMMAND}"
 ${COMMAND}
