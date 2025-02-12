52d882dad9f092705da6bb9eb91dcbb5f981dbb3
cjknight <cjknight2009@gmail.com>
2025-02-12 21:22:00 +0000

    add hostname to output

--- Examples/Polaris/python/hello_world.py
+++ Examples/Polaris/python/hello_world.py
@@ -1,11 +1,12 @@
 from mpi4py import MPI
 import sys
+import socket
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-print("Hello World from rank {} of {}".format(rank, size))
+print("Hello World from rank {} of {} on {}".format(rank, size, socket.gethostname()))
 
 sys.exit()
 
