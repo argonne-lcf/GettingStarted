82e1b650b322d5d4731879e293d29b8f6596471d
cjknight <cjknight2009@gmail.com>
2026-02-24 15:44:27 -0600

    remove cooley, theta, and thetagpu

--- ProgrammingModels/ThetaGPU/CUDA/vecadd/submit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -l
-#COBALT -n 1 -t 15 -q single-gpu -A Catalyst --attrs filesystems=home,grand,eagle
-
-mpirun -n 1 ./vecadd
-
--- ProgrammingModels/ThetaGPU/OpenCL/vecadd/submit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -l
-#COBALT -n 1 -t 15 -q single-gpu -A Catalyst --attrs filesystems=home,grand,eagle
-
-mpirun -np 1 ./vecadd
-
