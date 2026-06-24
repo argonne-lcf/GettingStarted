6f1497571a9e1a94950b2949465596d87535857b
harikrishna1410 <harikrishna1410@gmail.com>
2026-06-24 15:50:47 +0000

    commiting the submit script as is

--- AI_ML/LLM_Inference/EL/sub_el_request_aurora_llama8B.sh
+++ AI_ML/LLM_Inference/EL/sub_el_request_aurora_llama8B.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-##PBS -N el-llm
-##PBS -l select=2
-##PBS -l walltime=00:30:00
-##PBS -q debug-scaling
-##PBS -A <project_name>
-##PBS -l filesystems=home:flare
-##PBS -j oe
-#cd $PBS_O_WORKDIR
+#PBS -N el-llm
+#PBS -l select=2
+#PBS -l walltime=00:30:00
+#PBS -q debug
+#PBS -A datascience
+#PBS -l filesystems=home:flare
+#PBS -j oe
+cd $PBS_O_WORKDIR
 
 set -e
 
