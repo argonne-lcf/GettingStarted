89c91d5ca967e245845880e0416d53a6cfca6934
Riccardo Balin <riccardo.balin@gmail.com>
2026-07-10 22:42:38 +0000

    Switch to EL main branch

--- AI_ML/LLM_Inference/EL/sub_el_batched_polaris_llama8B.sh
+++ AI_ML/LLM_Inference/EL/sub_el_batched_polaris_llama8B.sh
@@ -40,7 +40,6 @@ source /tmp/_env/bin/activate
 cd /tmp
 git clone https://github.com/argonne-lcf/ensemble_launcher.git
 cd ensemble_launcher
-git checkout multi_node_vllm # NB: to remove, things will be merged into main
 pip install .
 cd $PBS_O_WORKDIR
 mpiexec -np "${NODES}" -ppn 1 --cpu-bind numa ./bcast --no-root-write \
